20e6924a7ddcdee6f4e098d5bbb295f8dacc23f2
wickstargazer <wickyorama@gmail.com>
2022-04-10 13:38:24 +0700

    (fix): added meta data resolver

--- src/Lighthouse/Program.NetCore.cs
+++ src/Lighthouse/Program.NetCore.cs
@@ -5,6 +5,8 @@
 // -----------------------------------------------------------------------
 
 using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace Lighthouse
 {
@@ -13,6 +15,18 @@ namespace Lighthouse
 #if CORECLR
         public static void Main(string[] args)
         {
+            string metadataString = string.Empty;
+            var ecsEndpoint = Environment.GetEnvironmentVariable("ECS_CONTAINER_METADATA_URI_V4");
+            using (var httpClient = new HttpClient())
+            {
+                var response = httpClient.GetAsync(Environment.GetEnvironmentVariable("ECS_CONTAINER_METADATA_URI_V4") + "/taskWithTags").Result;
+                metadataString = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine("ECS Metadata: " + metadataString);
+                var metadata = JObject.Parse(metadataString);
+                var taskIdSplit = metadata["TaskARN"].ToString().Split("/");
+                var taskId = taskIdSplit[taskIdSplit.Length - 1];
+                Environment.SetEnvironmentVariable("CLUSTER_IP", $"{taskId}.lighthouse.batch-import-system");
+           }
             var lighthouseService = new LighthouseService();
             lighthouseService.Start();
             Console.WriteLine("Press Control + C to terminate.");
